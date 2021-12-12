7b3f6c248ebf8bced3c74b14ba0f447a2d28704a
AndrewTheArtist <a.sutt@khk.ee>
2021-12-12 18:12:44 +0200

    Got everything Working but how to display data is a new problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
 import React from 'react';
 import './App.css';
 
-const App: React.FC = () => {
+const App = () => {
 
     //Get User Position Basically
-    navigator.geolocation.getCurrentPosition(function(position) {
+    navigator.geolocation.getCurrentPosition(function (position) {
         console.log("Latitude is :", position.coords.latitude);
         console.log("Longitude is :", position.coords.longitude);
-        // @ts-ignore
+
         let lat = position.coords.latitude
         let lon = position.coords.longitude
         let key = '4346a37bbdbf2e62faf141451f8b3459'
-        let url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
+        let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
         fetch(url)
             .then(response => {
-                if(!response.ok){
+                if (!response.ok) {
                     throw new Error(response.statusText)
                 }
                 return response.json();
             })
-            .then(data =>{
-                console.log(data);
+            .then(data => {
+               console.log(data)
             })
             .catch(console.error)
     });
-    return(
+    return (
         <div>
-            <h1>Test this out</h1>
+            <h1>Test Test 123 123</h1>
+            <h2>This is an noew hets</h2>
         </div>
     )
 
